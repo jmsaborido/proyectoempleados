48bd213edfe80bb99144c37f90ad56825629885e
Jose Maria Saborido <josemaria.saborido@iesdonana.org>
2020-05-17 18:59:25 +0200

    Arreglo cosas innecesarias segun el sonarlint

--- src/main/java/com/prueba/proyectoempleados/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/prueba/proyectoempleados/service/impl/EmployeeServiceImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.Date;
-import java.util.List;import java.util.Optional;
+import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
