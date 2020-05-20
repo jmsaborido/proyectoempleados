b51458af5be9a468a58fa2591b664c1a12751426
Jose Maria Saborido <josemaria.saborido@iesdonana.org>
2020-05-20 11:30:25 +0200

    cambio P por p en pom.xml en la dependencia lombok

--- src/main/java/com/prueba/proyectoempleados/model/DeptEmpPK.java
+++ src/main/java/com/prueba/proyectoempleados/model/DeptEmpPK.java
@@ -3,7 +3,6 @@ package com.prueba.proyectoempleados.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
